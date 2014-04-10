d0be2ca710116517c36b2c7abf0368f694991964
Colin Stodd <cbstodd@gmail.com>
2014-04-09 22:00:00 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ColinStodd::Application.config.secret_token = '65a1e1e0629d9dcbf785a71d6c375fe900d2c250fa235b14905a50051050f0107fa2d3f72ce9e37444febdfabf730de5f5ee8cec908ebdad04e65bb29812628e'
