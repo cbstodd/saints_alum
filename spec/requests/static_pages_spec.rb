237f41e671b7062c9613b81bad501bbd53987482
Colin Stodd <cbstodd@gmail.com>
2014-04-10 14:41:39 -0400

    Refacture Rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,22 +6,23 @@ describe "Static pages" do
   
   describe "Home page" do
     before { visit root_path }
-   it { should have_content("Welcome to my") }
-   it { should have_title("colinstodd.com") }
-   it { should_not have_title("Home |") }
+    it { should have_title("colinstodd.com") }
+    it { should_not have_title("Home |") }
+    it { should have_selector("h2", text: "Welcome to my") }
   end
 
   describe "About page" do
     before { visit about_path }
-    it { should have_content("About") }
-    it { should have_title("About") }   
+    it { should have_title("About") }  
+    it { should have_selector("h1", text: "About") } 
   end
 
   describe "Contact page" do
     before { visit contact_path }
-    it { should have_content("Contact") }
     it { should have_title("Contact") }
+    it { should have_selector("h1", text: "Contact") }
   end
 
-  
+
+
 end
