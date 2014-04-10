c4b4e4691787e0c0cde628403e56b50c33f3c325
Colin Stodd <cbstodd@gmail.com>
2014-04-09 22:25:30 -0400

    Add precompiled assets for Heroku

--- config/application.rb
+++ config/application.rb
@@ -60,8 +60,10 @@ module ColinStodd
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = false
+    config.assets.enabled = true
 
+    config.assets.initialize_on_precompile = false
+    
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
